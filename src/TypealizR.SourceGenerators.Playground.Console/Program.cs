6f322deee7982e07c52f0174189d4fd522844a72
earloc <earloc@live.de>
2022-11-12 11:08:18 +0100

    ignore the fact that new Program.cs are ending up in the global::-namespace, for now

--- src/TypealizR.SourceGenerators.Playground.Console/Program.cs
+++ src/TypealizR.SourceGenerators.Playground.Console/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using TypealizR.SourceGenerators.Playground.Console;
 
 var services = new ServiceCollection();
 services.AddLocalization();
 var provider = services.BuildServiceProvider();
 
-var localize = provider.GetRequiredService<IStringLocalizer<Program>>();
+var localize = provider.GetRequiredService<IStringLocalizer<App>>();
+
+
+Console.WriteLine(localize.Hello_World());
