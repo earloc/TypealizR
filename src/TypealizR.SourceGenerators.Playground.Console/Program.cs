b49190442f2c27d5d2385d690af649d2a4658f0c
earloc <earloc@live.de>
2022-11-12 11:01:26 +0100

    add console-playground

---
+++ src/TypealizR.SourceGenerators.Playground.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+var services = new ServiceCollection();
+services.AddLocalization();
+var provider = services.BuildServiceProvider();
+
+var localize = provider.GetRequiredService<IStringLocalizer<Program>>();
