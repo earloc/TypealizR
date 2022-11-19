6dad39da99d86ede09705e81de8b0e393c97adf7
earloc <earloc@live.de>
2022-11-19 15:42:51 +0100

    prepare test-case(s)

---
+++ src/TypealizR.SourceGenerators.Tests/DiagnosticsFactory.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+
+namespace TypealizR.SourceGenerators.Tests;
+
+public class DiagnosticsFactoryTests
+{
+
+	private DiagnosticsFactory CreateSut(DiagnosticsId id) => new("someFile.resx", "someKey", 42);
+
+
+	[Fact]
+	public void DoestNot_Emit_Warning_When_Disabled_TR0002 ()
+	{
+		var sut = CreateSut(DiagnosticsFactory.TR0001);
+
+		var diagnostic = sut.AmbigiousRessourceKey_0002("fallback");
+		diagnostic.DefaultSeverity.Should().Be(Microsoft.CodeAnalysis.DiagnosticSeverity.Info);
+	}
+}
