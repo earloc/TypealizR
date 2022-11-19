15e8c4531b5fc1c25246d6a2c4d6632416874adb
earloc <earloc@live.de>
2022-11-19 20:06:19 +0100

    remove static dictionary

--- src/TypealizR.SourceGenerators/DiagnosticsFactory.cs
+++ src/TypealizR.SourceGenerators/DiagnosticsFactory.cs
@@ -12,34 +12,15 @@ internal class DiagnosticsFactory
 	private readonly int lineNumber;
 	private readonly IDictionary<string, DiagnosticSeverity> severityMap;
 
-	public static Dictionary<string, DiagnosticSeverity> DefaultSeverityMap = new()
-	{
-		{   nameof(TR0001), DiagnosticSeverity.Error },
-		{   nameof(TR0002), DiagnosticSeverity.Warning },
-		{   nameof(TR0003), DiagnosticSeverity.Warning },
-		{   nameof(TR0004), DiagnosticSeverity.Warning },
-	};
-
-	public DiagnosticsFactory(string filePath, string rawRessourceKey, int lineNumber, IDictionary<string, DiagnosticSeverity> map)
+	public DiagnosticsFactory(string filePath, string rawRessourceKey, int lineNumber, IDictionary<string, DiagnosticSeverity> severityMap)
 	{
 		this.filePath = filePath;
 		this.rawRessourceKey = rawRessourceKey;
 		this.lineNumber = lineNumber;
-
-		this.severityMap = new Dictionary<string, DiagnosticSeverity>();
-
-		foreach (var key in DefaultSeverityMap.Keys)
-		{
-			if (!map.ContainsKey(key))
-			{
-				this.severityMap[key] = DefaultSeverityMap[key];
-			}
-			else
-			{
-				this.severityMap[key] = map[key];
-			}
-		}
+		this.severityMap = severityMap;
 	}
+
+	private DiagnosticSeverity? SeverityFor(string code) => severityMap.ContainsKey(code) ? severityMap[code] : default;
 
 	internal static readonly DiagnosticsId TR0001 = new(nameof(TR0001), "TargetProjectRootDirectoryNotFound");
 	internal static Diagnostic TargetProjectRootDirectoryNotFound_0001() =>
@@ -48,7 +29,7 @@ internal class DiagnosticsFactory
 				title: TR0001.Title,
 				messageFormat: "The code generator could not determine the projects root-directory. See {0}",
 				category: "Project",
-				defaultSeverity: DefaultSeverityMap[TR0001.Code],
+				defaultSeverity: DiagnosticSeverity.Error,
 				isEnabledByDefault: true,
 				description: "The code generator could not determine the projects root-directory",
 				helpLinkUri: DiagnosticsId.LinkToDocs(TR0001)
@@ -63,7 +44,7 @@ internal class DiagnosticsFactory
 				title: TR0002.Title,
 				messageFormat: "Ressource contains the key '{0}' that would end up as a duplicate method-name. Using '{1}' as derived name for this key. See {2}",
 				category: "Readability",
-				defaultSeverity: severityMap[TR0002.Code],
+				defaultSeverity: SeverityFor(TR0002.Code) ?? DiagnosticSeverity.Warning,
 				isEnabledByDefault: true,
 				description: "Encountered an ambigious ressource-key",
 				helpLinkUri: DiagnosticsId.LinkToDocs(TR0002)
@@ -85,7 +66,7 @@ internal class DiagnosticsFactory
 				title: TR0003.Title,
 				messageFormat: "Ressource-key '{0}' uses the generic format-parameter '{1}'. Consider to to use a more meaningful name, instead. See {2}",
 				category: "Readability",
-				defaultSeverity: severityMap[TR0003.Code],
+				defaultSeverity: SeverityFor(TR0003.Code) ?? DiagnosticSeverity.Warning,
 				isEnabledByDefault: true,
 				description: "Encountered a generic parameter",
 				helpLinkUri: DiagnosticsId.LinkToDocs(TR0003)
@@ -108,7 +89,7 @@ internal class DiagnosticsFactory
 				title: TR0004.Title,
 				messageFormat: "Ressource-key '{0}' uses unrecognized parameter-annotation '{1}'. Falling back to 'object'. See {2}",
 				category: "Readability",
-				defaultSeverity: severityMap[TR0004.Code],
+				defaultSeverity: SeverityFor(TR0004.Code) ?? DiagnosticSeverity.Warning,
 				isEnabledByDefault: true,
 				description: "Encountered an unrecognized parameter-type",
 				helpLinkUri: DiagnosticsId.LinkToDocs(TR0004)
