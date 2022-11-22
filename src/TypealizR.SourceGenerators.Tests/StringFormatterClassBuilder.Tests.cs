4486bcf5b0ca446167e042c766dc8a281fd8dd33
earloc <earloc@live.de>
2022-11-22 22:28:24 +0100

    add test for user-provided implementation #41

--- src/TypealizR.SourceGenerators.Tests/StringFormatterClassBuilder.Tests.cs
+++ src/TypealizR.SourceGenerators.Tests/StringFormatterClassBuilder.Tests.cs
@@ -8,12 +8,12 @@ public class StringFormatterClassBuilder_Tests
 {
 
 	[Fact]
-	public void Generates_DefaultImplementation_When_UserImplementation_Is_Not_Provided()
+	public void Generates_DefaultImplementation_When_UserImplementation_Is_NOT_Provided()
 	{
 		var sut = new StringFormatterClassBuilder("Some.Name.Space");
 		sut.UserModeImplementationIsProvided();
 
-		var actual = sut.Build().Split("\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.RemoveEmptyEntries);
+		var actual = sut.Build().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.RemoveEmptyEntries);
 
 		var expected = $@"
 namespace global::Some.Name.Space {{
@@ -29,6 +29,28 @@ namespace global::Some.Name.Space {{
 	internal static partial class TypealizR_StringFormatter
 	{{
 		public static partial string Format(this LocalizedString s, params object[] args) => string.Format(global::System.Globalization.CultureInfo.CurrentCulture, s, args)
+	}}
+}}
+".Trim().Split("\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.RemoveEmptyEntries);
+
+		actual.Should().BeEquivalentTo(expected);
+
+	}
+
+	[Fact]
+	public void DoesNot_Generates_DefaultImplementation_When_UserImplementation_IS_Provided()
+	{
+		var sut = new StringFormatterClassBuilder("Some.Name.Space");
+
+		var actual = sut.Build().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.RemoveEmptyEntries);
+
+		var expected = $@"
+namespace global::Some.Name.Space {{
+	{_.GeneratedCodeAttribute}
+	[DebuggerStepThrough]
+	internal static partial class TypealizR_StringFormatter
+	{{
+		public static partial string Format(this LocalizedString s, params object[] args);
 	}}
 }}
 ".Trim().Split("\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.RemoveEmptyEntries);
