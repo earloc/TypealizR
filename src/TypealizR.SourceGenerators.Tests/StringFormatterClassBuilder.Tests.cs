16f3a17137e26a6ddab5b6310bea2127589d4cd9
earloc <earloc@live.de>
2022-11-22 22:00:54 +0100

    prepare test for extension-point to support customized string-formatting #41

---
+++ src/TypealizR.SourceGenerators.Tests/StringFormatterClassBuilder.Tests.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Globalization;
+using FluentAssertions;
+using TypealizR.SourceGenerators.Extensibility;
+
+namespace TypealizR.SourceGenerators.Tests;
+public class StringFormatterClassBuilder_Tests
+{
+
+	[Fact]
+	public void Generates_DefaultImplementation_When_UserImplementation_Is_Not_Provided()
+	{
+		var sut = new StringFormatterClassBuilder("Some.Name.Space");
+		sut.UserModeImplementationIsProvided();
+
+		var actual = sut.Build().Trim();
+
+		var expected = $@"
+namespace global::Some.Name.Space {{
+	{_.GeneratedCodeAttribute}
+	[DebuggerStepThrough]
+	internal static partial class TypealizR_StringFormatter
+	{{
+		public static partial string Format(this LocalizedString s, params object[] args);
+	}}
+
+	{_.GeneratedCodeAttribute}
+	[DebuggerStepThrough]
+	internal static partial class TypealizR_StringFormatter
+	{{
+		public static partial string Format(this LocalizedString s, params object[] args) => string.Format(global::System.Globalization.CultureInfo.CurrentCulture, s, args)
+	}}
+}}
+".Trim();
+
+		actual.Should().Be(expected);
+
+	}
+
+}
---
+++ src/TypealizR.SourceGenerators/Extensibility/StringFormatterClassBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypealizR.SourceGenerators.Extensibility;
+internal class StringFormatterClassBuilder
+{
+	private string rootNamespace;
+
+	public StringFormatterClassBuilder(string rootNamespace)
+	{
+		this.rootNamespace = rootNamespace;
+	}
+
+	private bool isUserModeImplementationProvided = false;
+	internal void UserModeImplementationIsProvided() => isUserModeImplementationProvided = true;
+
+	internal string Build()
+	{
+		var stringFormatterStub = "a";
+
+		var defaultImplementation = default(string?);
+		if (isUserModeImplementationProvided)
+		{
+			defaultImplementation = "b";
+		}
+
+		var builder = new StringBuilder();
+		builder.AppendLine(stringFormatterStub);
+		if (!string.IsNullOrEmpty(defaultImplementation))
+		{
+			builder.AppendLine(defaultImplementation);
+		}
+
+		return builder.ToString();
+	}
+}
