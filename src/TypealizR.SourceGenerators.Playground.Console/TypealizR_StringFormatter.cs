3e689a2b3d13a9cd4af2f88154e87f097f35ae1d
earloc <earloc@live.de>
2022-11-23 12:56:23 +0100

    provide custom-implementation of StringFormatter #41

---
+++ src/TypealizR.SourceGenerators.Playground.Console/TypealizR_StringFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+
+namespace TypealizR.SourceGenerators.Playground.Console;
+internal static partial class TypealizR_StringFormatter
+{
+	public static partial LocalizedString Format(this LocalizedString that, params object[] args)
+	{
+		var value = string.Format(that.Value, args);
+		return new(that.Name, new string(value.Reverse().ToArray()), that.ResourceNotFound, that.SearchedLocation);
+	}
+}
