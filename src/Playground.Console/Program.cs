b92e6d6b040c2c3af6ebb772b145f74f915c2c30
earloc <earloc@live.de>
2022-12-03 11:07:14 +0100

    add playground projects

---
+++ src/Playground.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Playground.Shared;
+
+Console.WriteLine("Hello, World!");
+
+var services = new ServiceCollection();
+services.AddLogging();
+services.AddLocalization();
+services.AddSingleton<Greeter, Greeter>();
+
+
+var provider = services.BuildServiceProvider();
+
+var greeter = provider.GetRequiredService<Greeter>();
+
+greeter.SayHello("Arthur");
+
---
+++ src/Playground.Shared/Greeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Localization;
+
+namespace Playground.Shared;
+public class Greeter
+{
+	private readonly IStringLocalizer<Greetings> localize;
+
+	public Greeter(IStringLocalizer<Greetings> localize)
+	{
+		this.localize = localize;
+	}
+
+	public void SayHello(string name) => Console.WriteLine(localize.Hello__name(name));
+}
