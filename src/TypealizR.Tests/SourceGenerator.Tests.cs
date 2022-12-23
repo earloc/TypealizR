062b209264a6ea45b4658cb9a7589ee7d321b963
earloc <earloc@live.de>
2022-12-23 18:34:13 +0100

    add snapshot test for TF0004

--- src/TypealizR.Tests/SourceGenerator.Tests.cs
+++ src/TypealizR.Tests/SourceGenerator.Tests.cs
@@ -49,6 +49,17 @@ public class SourceGenerator_Tests
 	}
 
 	[Fact]
+	public async Task Generates_Warning_TR0004()
+	{
+		await GeneratorTesterBuilder
+			.Create(BaseDirectory, RootNamespace)
+			.WithResxFile("TR0004_NoCode.resx")
+			.Build()
+			.Verify()
+		;
+	}
+
+	[Fact]
 	public async Task Generates_StringFormatter_WithDefaultImplementation_For_Empty_NoCode_Resx()
 	{
 		await GeneratorTesterBuilder
