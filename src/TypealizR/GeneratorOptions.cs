d09a64fc2193f680ae2e2912982717655b5a7afd
earloc <earloc@live.de>
2022-12-27 08:42:45 +0100

    pull up type

--- src/TypealizR/GeneratorOptions.cs
+++ src/TypealizR/GeneratorOptions.cs
@@ -8,72 +8,69 @@ using TypealizR.Diagnostics;
 
 namespace TypealizR;
 
-public partial class StringLocalizerSourceGenerator
+internal sealed class Options
 {
-	internal sealed class Options
-    {
-		public const string MSBUILD_PROJECT_DIRECTORY = "build_property.msbuildprojectdirectory";
-		public const string PROJECT_DIR = "build_property.projectdir";
-		public const string ROOT_NAMESPACE = "build_property.rootnamespace";
+	public const string MSBUILD_PROJECT_DIRECTORY = "build_property.msbuildprojectdirectory";
+	public const string PROJECT_DIR = "build_property.projectdir";
+	public const string ROOT_NAMESPACE = "build_property.rootnamespace";
 
-		public Options(string? projectDirectory, string rootNamespace, IDictionary<string, DiagnosticSeverity> severityConfig)
-        {
-            RootNamespace = rootNamespace;
-			SeverityConfig = severityConfig;
-			if (projectDirectory is not null)
-			{
-				ProjectDirectory = new DirectoryInfo(projectDirectory);
-			}
-        }
-
-        public DirectoryInfo? ProjectDirectory { get; }
-        public string RootNamespace { get; }
-		public IDictionary<string, DiagnosticSeverity> SeverityConfig { get; }
-
-		public static Options From(AnalyzerConfigOptions options)
+	public Options(string? projectDirectory, string rootNamespace, IDictionary<string, DiagnosticSeverity> severityConfig)
+	{
+		RootNamespace = rootNamespace;
+		SeverityConfig = severityConfig;
+		if (projectDirectory is not null)
 		{
-			if (!options.TryGetValue(MSBUILD_PROJECT_DIRECTORY, out var projectDirectory))
-			{
-				options.TryGetValue(PROJECT_DIR, out projectDirectory);
-			}
-
-			options.TryGetValue(ROOT_NAMESPACE, out var rootNamespace);
-
-			var severityConfig = ReadSeverityConfig(options);
-
-			return new(
-				projectDirectory: projectDirectory,
-				rootNamespace: rootNamespace ?? Guid.NewGuid().ToString(),
-				severityConfig: severityConfig
-			);
+			ProjectDirectory = new DirectoryInfo(projectDirectory);
 		}
-		private static IDictionary<string, DiagnosticSeverity> ReadSeverityConfig(AnalyzerConfigOptions options)
+	}
+
+	public DirectoryInfo? ProjectDirectory { get; }
+	public string RootNamespace { get; }
+	public IDictionary<string, DiagnosticSeverity> SeverityConfig { get; }
+
+	public static Options From(AnalyzerConfigOptions options)
+	{
+		if (!options.TryGetValue(MSBUILD_PROJECT_DIRECTORY, out var projectDirectory))
 		{
-			var severityConfig = new Dictionary<string, DiagnosticSeverity>();
+			options.TryGetValue(PROJECT_DIR, out projectDirectory);
+		}
 
-			var availableDiagnostics = Enum.GetValues(typeof(DiagnosticsId))
-				.OfType<DiagnosticsId>()
-				.Select(x => x.ToString()
-			);
+		options.TryGetValue(ROOT_NAMESPACE, out var rootNamespace);
 
-			foreach (var diagnostic in availableDiagnostics)
+		var severityConfig = ReadSeverityConfig(options);
+
+		return new(
+			projectDirectory: projectDirectory,
+			rootNamespace: rootNamespace ?? Guid.NewGuid().ToString(),
+			severityConfig: severityConfig
+		);
+	}
+	private static IDictionary<string, DiagnosticSeverity> ReadSeverityConfig(AnalyzerConfigOptions options)
+	{
+		var severityConfig = new Dictionary<string, DiagnosticSeverity>();
+
+		var availableDiagnostics = Enum.GetValues(typeof(DiagnosticsId))
+			.OfType<DiagnosticsId>()
+			.Select(x => x.ToString()
+		);
+
+		foreach (var diagnostic in availableDiagnostics)
+		{
+			var key = $"dotnet_diagnostic_{diagnostic.ToLower()}_severity";
+
+			if (options.TryGetValue(key, out var rawValue))
 			{
-				var key = $"dotnet_diagnostic_{diagnostic.ToLower()}_severity";
-
-				if (options.TryGetValue(key, out var rawValue))
+				if (Enum.TryParse<DiagnosticSeverity>(rawValue, true, out var severity))
 				{
-					if (Enum.TryParse<DiagnosticSeverity>(rawValue, true, out var severity))
-					{
-						severityConfig[diagnostic] = severity;
-					}
-					else
-					{
-						throw new InvalidOperationException($"'{key}' has invalid value '{rawValue}'");
-					}
+					severityConfig[diagnostic] = severity;
+				}
+				else
+				{
+					throw new InvalidOperationException($"'{key}' has invalid value '{rawValue}'");
 				}
 			}
-
-            return severityConfig;
 		}
+
+		return severityConfig;
 	}
 }
