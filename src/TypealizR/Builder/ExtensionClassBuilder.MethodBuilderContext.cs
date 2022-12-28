4831c6e12b92b4ce2cd22ac80c7e091eee67b9ea
earloc <earloc@live.de>
2022-12-28 14:31:01 +0100

    make type reusable

--- src/TypealizR/Builder/ExtensionClassBuilder.MethodBuilderContext.cs
+++ src/TypealizR/Builder/ExtensionClassBuilder.MethodBuilderContext.cs
@@ -1,17 +1,14 @@
 ﻿using TypealizR.Diagnostics;
 
 namespace TypealizR.Builder;
-internal partial class ExtensionClassBuilder
+internal sealed class MethodBuilderContext
 {
-	private sealed class MethodBuilderContext
+	public MethodBuilderContext(ExtensionMethodBuilder builder, DiagnosticsCollector diagnostics)
 	{
-		public MethodBuilderContext(ExtensionMethodBuilder builder, DiagnosticsCollector diagnostics)
-		{
-			Builder = builder;
-			Diagnostics = diagnostics;
-		}
-
-		public ExtensionMethodBuilder Builder { get; }
-		public DiagnosticsCollector Diagnostics { get; }
+		Builder = builder;
+		Diagnostics = diagnostics;
 	}
+
+	public ExtensionMethodBuilder Builder { get; }
+	public DiagnosticsCollector Diagnostics { get; }
 }
