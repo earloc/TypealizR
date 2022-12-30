e2aedcf94cfcbf7c29bbe2fa541be79b94483e51
earloc <earloc@live.de>
2022-12-30 18:03:36 +0100

    move files around

---
+++ src/TypealizR/Core/TypeModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace TypealizR.Builder;
+public class TypeModel
+{
+    public TypeModel(string @namespace, string name, Accessibility accessibility = Accessibility.Internal)
+    {
+        Namespace = @namespace;
+        Name = name;
+        Accessibility = accessibility;
+    }
+
+    public string Namespace { get; }
+    public string Name { get; }
+
+    public Accessibility Accessibility { get; }
+
+    public string FullName => $"{Namespace}.{Name}";
+    public string GlobalFullName => $"global::{FullName}";
+
+    public string FullNameForClassName => FullName.Replace(".", "");
+
+
+}
