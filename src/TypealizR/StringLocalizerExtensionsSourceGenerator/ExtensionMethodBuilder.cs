f668ebeaa533a808d89f7466065d40e2f7ca1fd2
earloc <earloc@live.de>
2023-01-15 22:47:14 +0100

    formatting

--- src/TypealizR/StringLocalizerExtensionsSourceGenerator/ExtensionMethodBuilder.cs
+++ src/TypealizR/StringLocalizerExtensionsSourceGenerator/ExtensionMethodBuilder.cs
@@ -10,28 +10,37 @@ using Microsoft.CodeAnalysis;
 using System.Xml.Linq;
 using TypealizR.Extensions;
 using TypealizR.Diagnostics;
-using TypealizR.Core;namespace TypealizR;
+using TypealizR.Core;
+
+namespace TypealizR;
 internal class ExtensionMethodBuilder
 {
 	private readonly TypeModel markerType;
 	private readonly string key;
 	private readonly string value;
-	private readonly DiagnosticsCollector diagnostics;    private readonly bool useParametersInMethodNames;    private readonly ParameterBuilder parameterBuilder;
+	private readonly DiagnosticsCollector diagnostics;
+    private readonly bool useParametersInMethodNames;
+    private readonly ParameterBuilder parameterBuilder;
 
 	public ExtensionMethodBuilder(bool useParametersInMethodNames, TypeModel markerType, string key, string value, DiagnosticsCollector diagnostics)
 	{
-        this.useParametersInMethodNames = useParametersInMethodNames;		this.markerType = markerType;
+        this.useParametersInMethodNames = useParametersInMethodNames;
+		this.markerType = markerType;
 		this.key = key;
 		this.value = value;
-		this.diagnostics = diagnostics;        parameterBuilder = new(key);
+		this.diagnostics = diagnostics;
+        parameterBuilder = new(key);
 	}
 
 	public ExtensionMethodModel Build()
 	{
-		var parameters = parameterBuilder.Build(diagnostics);        string sanitizedMethodName = key;
+		var parameters = parameterBuilder.Build(diagnostics);
+        string sanitizedMethodName = key;
 
 		foreach (var parameter in parameters)
-		{            var sanitizedParameterName = useParametersInMethodNames ? $"_{parameter.Name}_" : "";            sanitizedMethodName = sanitizedMethodName.Replace(parameter.Token, sanitizedParameterName);
+		{
+            var sanitizedParameterName = useParametersInMethodNames ? $"_{parameter.Name}_" : "";
+            sanitizedMethodName = sanitizedMethodName.Replace(parameter.Token, sanitizedParameterName);
 		}
 
 		var name = new MemberName(sanitizedMethodName.Trim());
