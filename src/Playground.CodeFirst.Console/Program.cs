ac1cdff45a6c441de9058eb6ca729029727a4a66
earloc <earloc@live.de>
2023-01-17 23:52:26 +0100

    bootstrap projects

---
+++ src/Playground.CodeFirst.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using TypealizR.CodeFirst.Abstractions;
+
+Console.WriteLine("Hello, World!");
+
+ILocalizables i18n = new Localizables();
+
+Console.WriteLine(i18n.Hello(world:"Earth"));
+
+[TypealizR]
+interface ILocalizables
+{
+    /// <summary>
+    /// Hello <paramref name="world"/>
+    /// </summary>
+    /// <param name="world"></param>
+    /// <returns></returns>
+    string Hello(string world);
+}
+
+partial class Localizables : ILocalizables
+{
+    public string Hello(string world) => $"Hello {world}";
+}
---
+++ src/TypealizR.CodeFirst.Abstractions/TypealizRAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace TypealizR.CodeFirst.Abstractions;
+
+[AttributeUsage(AttributeTargets.Interface)]
+public class TypealizRAttribute : Attribute
+{
+}
