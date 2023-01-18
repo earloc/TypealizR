62fba24e21592f00083909ee5566ffc17bc46121
earloc <earloc@live.de>
2023-01-18 19:53:56 +0100

    add marker attribute

--- src/TypealizR.CodeFirst.Abstractions/CodeFirstTypealizedAttribute.cs
+++ src/TypealizR.CodeFirst.Abstractions/CodeFirstTypealizedAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace TypealizR.CodeFirst.Abstractions;
 
 [AttributeUsage(AttributeTargets.Interface)]
-public class TypealizRAttribute : Attribute
+public class CodeFirstTypealizedAttribute : Attribute
 {
 }
