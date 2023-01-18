293116620fd37fecda2b2c40aa5b8ddac8af6c2b
earloc <earloc@live.de>
2023-01-19 00:09:01 +0100

    find relevant members

--- src/TypealizR.Tests/CodeFirstSourceGenerator.Tests/Interfaces.cs
+++ src/TypealizR.Tests/CodeFirstSourceGenerator.Tests/Interfaces.cs
@@ -1,10 +1,16 @@
-﻿using TypealizR.CodeFirst.Abstractions;
+﻿using Microsoft.Extensions.Localization;
+using TypealizR.CodeFirst.Abstractions;
 
 namespace TypealizR.Tests.CodeFirst;
 
 [CodeFirstTypealized]
 internal interface IInterfaceWithMarker
 {
+    bool IsEnabledProperty { get; }
+    bool IsEnabledMethod();
+
+    LocalizedString Greeting { get; }
+    LocalizedString Hello(string world);
 }
 
 internal interface IInterfaceWithoutMarker
