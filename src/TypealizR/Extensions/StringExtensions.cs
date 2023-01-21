5c4c0836fafdc36f697e128e022e06eeef26d40a
earloc <earloc@live.de>
2023-01-21 16:49:24 +0100

    chore: formatting

--- src/TypealizR/Extensions/StringExtensions.cs
+++ src/TypealizR/Extensions/StringExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Linq;using System.Text;
-using TypealizR.Extensions;namespace System;
+using System.Linq;
+using System.Text;
+using TypealizR.Extensions;
+
+namespace System;
 internal static class StringExtensions
 {
 	public static string ToMultiline(this IEnumerable<string> that, string prependLinesWith = "", bool appendNewLineAfterEach = true)
@@ -25,8 +28,14 @@ internal static class StringExtensions
 	}
 
 	public static string ToCommaDelimited(this IEnumerable<string> that) 
-        => string.Join(", ", that);
+        => string.Join(", ", that)
+    ;
 
     public static string RemoveAndReplaceDuplicatesOf(this string s, string seperator, string join) 
-        => string.Join(join, s.Split(new[] { seperator }, StringSplitOptions.RemoveEmptyEntries));    public static string ReplaceInvalidForMemberNameWith(this string that, char replacement)        => new(that.Select(x => x.IsValidInIdentifier(false) ? x : replacement).ToArray())    ;   
+        => string.Join(join, s.Split(new[] { seperator }, StringSplitOptions.RemoveEmptyEntries))
+    ;
+
+    public static string ReplaceInvalidForMemberNameWith(this string that, char replacement)
+        => new(that.Select(x => x.IsValidInIdentifier(false) ? x : replacement).ToArray())
+    ;
 }
