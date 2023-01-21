da79abc9aca75bb57c28256ef2cddc1f5f201bcb
earloc <earloc@live.de>
2023-01-21 18:25:23 +0100

    feature: add parameter for default value

--- src/TypealizR/CodeFirstSourceGenerator/CodeFirstPropertyBuilder.cs
+++ src/TypealizR/CodeFirstSourceGenerator/CodeFirstPropertyBuilder.cs
@@ -5,14 +5,16 @@ namespace TypealizR;
 internal class CodeFirstPropertyBuilder
 {
     private readonly string name;
+    private readonly string defaultValue;
 
-    public CodeFirstPropertyBuilder(string name)
+    public CodeFirstPropertyBuilder(string name, string? defaultValue)
     {
         this.name = name;
+        this.defaultValue = defaultValue ?? name;
     }
 
     internal CodeFirstPropertyModel Build()
     {
-        return new CodeFirstPropertyModel(name, "LocalizedString", name);
+        return new CodeFirstPropertyModel(name, "LocalizedString", defaultValue);
     }
 }
