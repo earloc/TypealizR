408c62248ce7633e268cedb1e89ab0bcf5ef7d9a
earloc <earloc@live.de>
2023-01-23 16:32:51 +0100

    demo: provide sample usage of code-first-i18n

---
+++ src/Playground.CodeFirst.Console/ILocalizables.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Localization;
+using TypealizR.CodeFirst.Abstractions;
+
+namespace TypealizR.CodeFirst.Console;
+
+[CodeFirstTypealized]
+public interface ILocalizables
+{
+    ///<summary>
+    ///asdasdasd
+    ///</summary>
+    ///<param name="world"></param>
+    ///<returns></returns>
+    LocalizedString Hello(string world);
+
+    ///<summary>
+    ///Goodbye, <paramref name="user"/>
+    ///</summary>
+    ///<param name="user"></param>
+    ///<returns></returns>
+    LocalizedString Farewell(string user);
+
+    ///<summary>
+    ///42
+    ///</summary>
+    LocalizedString WhatIsTheMeaningOfLifeTheUniverseAndEverything { get; }
+}
--- src/Playground.CodeFirst.Console/Program.cs
+++ src/Playground.CodeFirst.Console/Program.cs
@@ -1,30 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.ComponentModel;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Localization;
-using TypealizR.CodeFirst.Abstractions;
+using TypealizR.CodeFirst.Console;
 
 var services = new ServiceCollection();
+services.AddLogging();
+services.AddLocalization();
+
+services.AddScoped<ILocalizables, Localizables>();
 var provider = services.BuildServiceProvider();
 using var scope = provider.CreateScope();
-
 
 var i18n = scope.ServiceProvider.GetRequiredService<ILocalizables>();
 Console.WriteLine(i18n.Hello("Earth"));
 
-
-[CodeFirstTypealized]
-interface ILocalizables
-{
-    ///<summary>
-    /// Hello '<paramref name="world"/>'
-    ///</summary>
-    /// <param name="world">the name of the world to be greeted</param>
-    /// <returns></returns>
-    LocalizedString Hello(string world);
-}
-
-//partial class Localizables : ILocalizables
-//{
-//    public LocalizedString Hello(string world) => localizer["Hello '{0}'", world];
-//}
+Console.WriteLine(i18n.Farewell("Arthur"));
+Console.WriteLine(i18n.WhatIsTheMeaningOfLifeTheUniverseAndEverything);
