1011902f1315f8f88a64051cb1e116d68f3c1a3b
earloc <earloc@live.de>
2023-01-26 21:54:31 +0100

    feature: add cli

---
+++ src/TypealizR.CLI/Commands/CodeFirst/ExportCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine.Invocation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace TypealizR.CLI.Commands.CodeFirst;
+internal class ExportCommand
+{
+    public static async Task Handle(string baseDirectory, CancellationToken cancellationToken)
+    {
+        var directory = new DirectoryInfo(baseDirectory);
+
+        if (!MSBuildLocator.IsRegistered)
+        {
+            MSBuildLocator.RegisterDefaults();
+        }
+
+        foreach (var projectFile in directory.GetFiles("*.csproj"))
+        {
+            await ExportAsync(projectFile, cancellationToken);
+        }
+    }
+
+    private static async Task ExportAsync(FileInfo projectFile, CancellationToken cancellationToken)
+    {
+        using var w = MSBuildWorkspace.Create();
+        
+        var project = await w.OpenProjectAsync (projectFile.FullName, cancellationToken:cancellationToken);
+
+        await ExportAsync(project, cancellationToken);
+
+    }
+    private const string MarkerAttributeName = "CodeFirstTypealized";
+
+    private static async Task ExportAsync(Project project, CancellationToken cancellationToken)
+    {
+        var compilation = await project.GetCompilationAsync(cancellationToken);
+
+        if (compilation is null)
+        {
+            return;
+        }
+
+        var allInterfaces = compilation.SyntaxTrees
+            .Where(x => x.GetRoot() is CompilationUnitSyntax)
+            .Select(x => (CompilationUnitSyntax)x.GetRoot())
+            .SelectMany(x => x.Members.OfType<BaseNamespaceDeclarationSyntax>())
+            .SelectMany(x => x.Members.OfType<InterfaceDeclarationSyntax>())
+            .Select(x => new { Declaration = x, Model = compilation.GetSemanticModel(x.SyntaxTree) })
+        ;
+
+        var markedInterfaces = allInterfaces
+            .Select((x, cancel) => new { x.Declaration, Model = x.Model.GetDeclaredSymbol(x.Declaration) })
+            .Where(x => x.Model is not null)
+            .Select((x, cancel) => new { x.Declaration, Model = x.Model! })
+            .Where(x => x.Model
+                .GetAttributes()
+                .Any(x => x.AttributeClass?.Name.StartsWith(MarkerAttributeName) ?? false)
+            )
+        ;
+
+        foreach (var markedInterface in markedInterfaces)
+        {
+            Console.WriteLine(markedInterface.Model.ToDisplayString());
+
+            foreach (var member in markedInterface.Declaration.Members)
+            {
+                Console.WriteLine(member.ToString());
+            }
+        }
+
+
+    }
+}
---
+++ src/TypealizR.CLI/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System.CommandLine;
+using TypealizR.CLI.Commands.CodeFirst;
+
+
+var directoryOption = new Option<string>("--directory");
+directoryOption.AddAlias("-d");
+
+var rootCommand = new RootCommand();
+
+var codeFirstCommand = new Command("code-first");
+codeFirstCommand.AddAlias("cf");
+
+rootCommand.AddCommand(codeFirstCommand);
+
+var exportCommand = new Command("export");
+exportCommand.AddAlias("ex");
+exportCommand.AddOption(directoryOption);
+exportCommand.SetHandler(
+    (directory) => ExportCommand.Handle(directory, CancellationToken.None),
+    directoryOption
+);
+
+codeFirstCommand.AddCommand(exportCommand);
+
+await rootCommand.InvokeAsync(args);
