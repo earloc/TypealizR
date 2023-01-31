c13b43852bd96e850e069fbf5d6fe0bed4180b76
earloc <earloc@live.de>
2023-01-31 21:04:45 +0100

    test: add sample project

---
+++ src/Tests.CLI.SingleInterface_SingleProperty/ISimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+using TypealizR.CodeFirst.Abstractions;
+
+namespace Tests.CLI.SingleInterface_SingleProperty;
+
+
+[CodeFirstTypealized]
+public interface ISimple
+{
+    /// <summary>
+    /// Some localized value
+    /// </summary>
+    public LocalizedString SomeValue { get; }
+}
