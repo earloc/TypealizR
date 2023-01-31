ffae384cba22b6d5045b53996f64ba91ea133a76
earloc <earloc@live.de>
2023-01-31 21:03:06 +0100

    feature: preapre for dependency-injectable invocation

--- src/TypealizR.CLI/App.cs
+++ src/TypealizR.CLI/App.cs
@@ -4,14 +4,24 @@ using System.CommandLine;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TypealizR.CLI.Abstractions;
+using TypealizR.CLI.Binders;
 using TypealizR.CLI.Commands.CodeFirst;
 
 namespace TypealizR.CLI;
 internal class App
 {
-    RootCommand rootCommand = new ();
-    public App()
+    public class Dependencies
+    {
+        public Func<IServiceProvider, IStorage> Storage = x => new FileStorage();
+    }
+
+    readonly RootCommand rootCommand = new ();
+    public App(Dependencies? dependencies = null)
 	{
+        dependencies ??= new();
+
         Console.OutputEncoding = Encoding.UTF8;
 
         var directoryOption = new Option<string>("--directory");
@@ -27,8 +37,8 @@ internal class App
         exportCommand.AddAlias("ex");
         exportCommand.AddOption(directoryOption);
         exportCommand.SetHandler(
-            (directory) => ExportCommand.Handle(directory, CancellationToken.None),
-            directoryOption
+            (directory, storage) => ExportCommand.Handle(directory, storage, CancellationToken.None),
+            directoryOption, new FuncBinder<IStorage>(dependencies.Storage)
         );
 
         codeFirstCommand.AddCommand(exportCommand);
