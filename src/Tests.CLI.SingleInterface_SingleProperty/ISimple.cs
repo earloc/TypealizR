b6354afadd3a444ecebfbce1c8686783b85c7bd4
earloc <earloc@live.de>
2023-02-01 16:11:57 +0100

    test: add test-lab project

--- src/Tests.CLI.SingleInterface_SingleProperty/ISimple.cs
+++ src/Tests.CLI.SingleInterface_SingleProperty/ISimple.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Localization;
-using TypealizR.CodeFirst.Abstractions;
 
 namespace Tests.CLI.SingleInterface_SingleProperty;
-
 
 [CodeFirstTypealized]
 public interface ISimple
