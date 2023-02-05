be3cbdfe2d6c1d53d1047db43823b74e6edc7d67
earloc <earloc@live.de>
2023-02-05 16:01:12 +0100

    docs: revised getting-started section

---
+++ samples/ConsoleSTS/CodeFirst/ILocalizables.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Localization;
+using TypealizR.CodeFirst.Abstractions;
+
+namespace ConsoleSTS.CodeFirst;
+
+[CodeFirstTypealized]
+public interface ILocalizables
+{
+    /// <summary>
+    /// Hello, fellow developer!
+    /// </summary>
+    public LocalizedString Hello { get; }
+
+    /// <summary>
+    /// Hey <paramref name="userName"/>, welcome to <paramref name="planetName"/> 👍!
+    /// </summary>
+    /// <param name="userName"></param>
+    /// <param name="planetName"></param>
+    /// <returns></returns>
+    public LocalizedString Greet(string userName, string planetName);
+}
--- samples/ConsoleSTS/Program.cs
+++ samples/ConsoleSTS/Program.cs
@@ -1,12 +1,19 @@
 ﻿using ConsoleSTS;
+using ConsoleSTS.CodeFirst;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using System.Text;
 
 var services = new ServiceCollection();
 services.AddLogging();
 services.AddLocalization();
-var provider = services.BuildServiceProvider();
+services.AddSingleton<ILocalizables, Localizables>();
 
+var provider = services.BuildServiceProvider();
+Console.OutputEncoding = Encoding.UTF8;
+
+
+//resource-first
 var localize = provider.GetRequiredService<IStringLocalizer<App>>();
 
 Console.WriteLine(localize.Hello_World());
@@ -18,3 +25,10 @@ Console.WriteLine(localize.Hello__UserName(UserName: "Arthur"));
 var today = DateOnly.FromDateTime(DateTime.Now);
 
 Console.WriteLine(localize.Hello__name__today_is__now(name: "Arthur", now: today));
+
+
+//code-first
+var codeFirst = provider.GetRequiredService<ILocalizables>();
+
+Console.WriteLine(codeFirst.Hello);
+Console.WriteLine(codeFirst.Greet("Arthur", "earth"));
