568b90d218667d4bfae62200cabc6ecf2081b3d5
earloc <earloc@live.de>
2023-02-14 06:20:14 +0100

    docs: simplify the code-first sample

--- src/Playground.CodeFirst.Console/ILocalizables.cs
+++ src/Playground.CodeFirst.Console/ILocalizables.cs
@@ -9,22 +9,17 @@ public interface ILocalizables
     LocalizedString Hello(string world);
 
     ///<summary>
-    ///42
+    /// 42
     ///</summary>
     LocalizedString WhatIsTheMeaningOfLifeTheUniverseAndEverything { get; }
 
     ///<summary>
-    ///Goodbye, <paramref name="user"/>
+    /// Goodbye, <paramref name="user"/>
     ///</summary>
-    ///<param name="user"></param>
-    ///<returns></returns>
     LocalizedString Farewell(string user);
 
     /// <summary>
     /// <paramref name="right"/> greets <paramref name="left"/>, and <paramref name="left"/> answers: "Hi!".
     /// </summary>
-    /// <param name="left"></param>
-    /// <param name="right"></param>
-    /// <returns></returns>
     LocalizedString Greet(string left, string right);
 }
--- src/Playground.CodeFirst.Console/ILocalizablesWithDefaults.cs
+++ src/Playground.CodeFirst.Console/ILocalizablesWithDefaults.cs
@@ -7,30 +7,23 @@ namespace Playground.CodeFirst.Console;
 public interface ILocalizablesWithDefaults
 {
     ///<summary>
-    ///42
+    /// 42
     ///</summary>
     LocalizedString WhatIsTheMeaningOfLifeTheUniverseAndEverything { get; }
 
     /// <summary>
     /// Greetings to you, {0}
     /// </summary>
-    /// <param name="world"></param>
-    /// <returns></returns>
     LocalizedString Hello(string world);
 
     ///<summary>
-    ///Goodbye, <paramref name="user"/>
+    /// Goodbye, <paramref name="user"/>
     ///</summary>
-    ///<param name="user"></param>
-    ///<returns></returns>
     LocalizedString Farewell(string user);
 
     /// <summary>
     /// <paramref name="right"/> greets <paramref name="left"/>, and <paramref name="left"/> answers: "Hi!".
     /// </summary>
-    /// <param name="left"></param>
-    /// <param name="right"></param>
-    /// <returns></returns>
     LocalizedString Greet(string left, string right);
 
 
