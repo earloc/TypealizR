90a84e09be8f26e11615f75186c2c27abf0d0fda
earloc <earloc@live.de>
2024-10-24 19:27:27 +0200

    dev: add analyzer samples

--- src/Playground.Console/Program.cs
+++ src/Playground.Console/Program.cs
@@ -108,3 +108,26 @@ Console.WriteLine(
 Console.WriteLine(
     g.Some.Deeply.Nested.Thing.Called.After.A.Monster.With_the__name("Chewbacca")
 );
+
+
+
+
+var localizer = provider.GetRequiredService<IStringLocalizer>();
+
+var a = localizer.Bar();
+var b = localizer.Bar("bar");
+var c = localizer.Foo("foo");
+var d = localizer.FooBar("fooBar");
+var bs = localizer.Bars("Arthur", 42);
+
+public static class IStringLocalizerExtensions
+{
+    public static LocalizedString Bar(this IStringLocalizer that) => that["Bar"];
+    public static LocalizedString Bar(this IStringLocalizer that, string value) => that["Bar", value];
+
+    public static LocalizedString Foo(this IStringLocalizer that, string value) => that["Foo {bar}", value];
+    public static LocalizedString FooBar(this IStringLocalizer that, string value) => that["FooBar {0}", value];
+
+    public static LocalizedString Bars(this IStringLocalizer that, string v1, int v2) => that["Bar {0} {1}", v1, v2];
+
+}
