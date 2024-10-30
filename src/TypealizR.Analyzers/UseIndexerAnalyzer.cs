449175bbfad6974eeddda37ff9af01999b82c175
earloc <earloc@live.de>
2024-10-30 08:42:53 +0100

    refactor: switch to 'Info'-severity per default

--- src/TypealizR.Analyzers/UseIndexerAnalyzer.cs
+++ src/TypealizR.Analyzers/UseIndexerAnalyzer.cs
@@ -19,7 +19,7 @@ public class UseIndexerAnalyzer : DiagnosticAnalyzer
     private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
     private const string Category = "Usage";
 
-    private static readonly DiagnosticDescriptor Rule = new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+    private static readonly DiagnosticDescriptor Rule = new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
 
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [Rule];
 
