51affeb24ca1cbf2d9a570d14a2b09a08d83ec10
earloc <earloc@live.de>
2025-03-23 00:08:56 +0100

    test: simpler expectation

--- src/TypealizR.Tests/CodeFirstSourceGenerator.Tests/Innerface.cs
+++ src/TypealizR.Tests/CodeFirstSourceGenerator.Tests/Innerface.cs
@@ -3,15 +3,11 @@ using TypealizR.CodeFirst.Abstractions;
 
 namespace TypealizR.Tests.CodeFirst;
 
-internal class SomeOuterClass {
+internal partial class SomeOuterClass {
 
     [CodeFirstTypealized]
     internal interface II18n
     {
-        LocalizedString Greeting { get; }
-
         LocalizedString Hello(string world);
-
-        LocalizedString Hello(string user, string world, int visitCount, bool dontPanic);
     }
 }
