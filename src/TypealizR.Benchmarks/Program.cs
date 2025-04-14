0d88c7620673fc95604d7a878c44e523d4e3efe1
earloc <earloc@live.de>
2025-04-14 07:49:49 +0200

    test: prepare benchmarks

--- src/TypealizR.Benchmarks/Program.cs
+++ src/TypealizR.Benchmarks/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run(typeof(Program).Assembly);
