b482aea7ab6932edeba9bed4e355ceed3c0014fc
earloc <earloc@live.de>
2025-07-07 05:37:16 +0200

    fix: generate valid xml-comment #292

--- src/TypealizR/Core/CommentModel.cs
+++ src/TypealizR/Core/CommentModel.cs
@@ -1,1 +1,25 @@
-﻿namespace TypealizR.Core;internal class CommentModel{    private readonly string rawResourceName;    private readonly string defaultValue;    public CommentModel(string rawResourceName, string defaultValue)    {        this.rawResourceName = rawResourceName;        this.defaultValue = defaultValue;    }    public string ToCSharp() => $"""            /// <summary>            /// Looks up a localized string similar to '{rawResourceName}'            /// </summary>            /// <returns>            /// A localized version of the current default value of '{defaultValue}'            /// </returns>    """;}
+﻿using System.Security;
+using System.Xml.Linq;
+
+namespace TypealizR.Core;
+internal class CommentModel
+{
+    private readonly string rawResourceName;
+    private readonly XText defaultValue;
+
+    public CommentModel(string rawResourceName, string defaultValue)
+    {
+        this.rawResourceName = rawResourceName;
+        this.defaultValue = new XText(defaultValue);
+    }
+
+    public string ToCSharp() => $"""
+
+            /// <summary>
+            /// Looks up a localized string similar to '{rawResourceName}'
+            /// </summary>
+            /// <returns>
+            /// A localized version of the current default value of '{defaultValue}'
+            /// </returns>
+    """;
+}
